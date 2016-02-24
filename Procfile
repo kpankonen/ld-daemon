e26955120fceb49d2586c7c4fe714320922d3d3d
John Kodumal <john@launchdarkly.com>
2016-02-23 21:24:18 -0800

    Add a procfile. Bump dependencies. Merge in config values from environment

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: ./ld-daemon
--- ld-daemon.go
+++ ld-daemon.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/imdario/mergo"
+	"github.com/kelseyhightower/envconfig"
 	ld "github.com/launchdarkly/go-client"
 	ldr "github.com/launchdarkly/go-client/redis"
 	"gopkg.in/gcfg.v1"
@@ -13,84 +15,108 @@ import (
 
 var VERSION = "DEV"
 
+const CONFIG_ARG = "config"
+
 type Config struct {
 	Redis struct {
-		Host string
-		Port int
+		Host string `envconfig:"redis_host"`
+		Port int    `envconfig:"redis_port"`
+		Uri  string `envconfig:"redis_uri"`
 	}
 	Main struct {
 		ApiKey      string
 		Prefix      string
-		ExitOnError bool
-		StreamUri   string
-		BaseUri     string
+		ExitOnError bool   `envconfig:"exit_on_error"`
+		StreamUri   string `envconfig:"stream_uri"`
+		BaseUri     string `envconfig:"base_uri"`
+		EventsUri   string `envconfig:"events_uri"`
 	}
 }
 
 var configFile string
 
-var client ld.LDClient
+var client *ld.LDClient
+
+func exists(file string) bool {
+	if _, err := os.Stat("/path/to/whatever"); err == nil {
+		return true
+	}
+	return false
+}
 
 func main() {
-	flag.StringVar(&configFile, "config", "/etc/ld-daemon.conf", "configuration file location")
+	flag.StringVar(&configFile, CONFIG_ARG, "/etc/ld-daemon.conf", "configuration file location")
 
 	flag.Parse()
 
-	var c Config
+	var envConfig, c Config
 
-	fmt.Printf("Starting LaunchDarkly daemon version %s with configuration file %s\n", formatVersion(VERSION), configFile)
+	var isConfigSet bool
+
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == CONFIG_ARG {
+			isConfigSet = true
+		}
+	})
+
+	if isConfigSet || exists(configFile) {
+		fmt.Printf("Starting LaunchDarkly daemon version %s with configuration file %s\n", formatVersion(VERSION), configFile)
+	} else {
+		fmt.Printf("Starting LaunchDarkly daemon version %s (no configuration file specified)\n", formatVersion(VERSION))
+	}
 
 	err := gcfg.ReadFileInto(&c, configFile)
 
-	if err != nil {
+	if err != nil && isConfigSet {
 		fmt.Println("Failed to read configuration file. Exiting.")
+		os.Exit(1)
+	}
+
+	envErr := envconfig.Process("ld", &envConfig.Main)
+
+	if envErr != nil {
+		fmt.Println("Invalid environment variable value: %+v", envErr)
+		os.Exit(1)
+	}
+	envErr = envconfig.Process("ld", &envConfig.Redis)
+	if envErr != nil {
+		fmt.Println("Invalid environment variable value: %+v", envErr)
+		os.Exit(1)
+	}
+
+	mergeErr := mergo.MergeWithOverwrite(&c, envConfig)
+	if mergeErr != nil {
+		fmt.Printf("Failed to merge environment variables into configuration: %+v", mergeErr)
 		os.Exit(1)
 	}
 
 	clientConfig := ld.DefaultConfig
 	clientConfig.Stream = true
-	clientConfig.FeatureStore = ldr.NewRedisFeatureStore(c.Redis.Host, c.Redis.Port, c.Main.Prefix)
-	clientConfig.StreamUri = c.Main.StreamUri
-	clientConfig.BaseUri = c.Main.BaseUri
-
-	client = ld.MakeCustomClient(c.Main.ApiKey, clientConfig)
-	client.InitializeStream()
-
-	init := make(chan bool)
-
-	go func() {
-		for {
-			if client.IsStreamInitialized() {
-				init <- true
-				break
-			}
-		}
-	}()
-
-loop:
-	for {
-		select {
-		case <-init:
-			fmt.Println("Initialized stream")
-			break loop
-		case <-time.After(time.Second * 10):
-			fmt.Println("Timed out connecting to stream")
-			if c.Main.ExitOnError {
-				os.Exit(1)
-			}
-		}
+	if c.Redis.Uri == "" {
+		clientConfig.FeatureStore = ldr.NewRedisFeatureStore(c.Redis.Host, c.Redis.Port, c.Main.Prefix, 0)
+	} else {
+		clientConfig.FeatureStore = ldr.NewRedisFeatureStoreFromUrl(c.Redis.Uri, c.Main.Prefix, 0)
+	}
+	if c.Main.StreamUri != "" {
+		clientConfig.StreamUri = c.Main.StreamUri
+	}
+	if c.Main.BaseUri != "" {
+		clientConfig.BaseUri = c.Main.BaseUri
+	}
+	if c.Main.EventsUri != "" {
+		clientConfig.EventsUri = c.Main.EventsUri
 	}
 
-	for {
-		time.Sleep(time.Second)
-		if client.IsStreamDisconnected() {
-			fmt.Println("Stream connection lost")
-			if c.Main.ExitOnError {
-				os.Exit(1)
-			}
-		}
+	client, err = ld.MakeCustomClient(c.Main.ApiKey, clientConfig, 2*time.Minute)
+
+	if err != nil && c.Main.ExitOnError {
+		os.Exit(1)
+	} else {
+		fmt.Println("LaunchDarkly connection initialized")
 	}
 
+	done := make(chan bool)
+	<-done // Block forever
 }
 
 func formatVersion(version string) string {
